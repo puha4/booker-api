b3eefd09dade4b7bfc6dfe331539aa819514aa02
Vitalii Puhach <vitalick.pugach@gmail.com>
2017-02-22 12:29:09 +0200

    added param fetchr

--- src/AppBundle/Controller/AppointmentController.php
+++ src/AppBundle/Controller/AppointmentController.php
@@ -2,19 +2,28 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Boardroom;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
 use Symfony\Component\HttpFoundation\Response;
 
 class AppointmentController extends FOSRestController
 {
-    public function getAppointmentsAction(Boardroom $boardroom)
+    /**
+     * @QueryParam(name="boardroom",  nullable=true, description="Boardroom Id.")
+     */
+    public function getAppointmentsAction(ParamFetcher $paramFetcher)
     {
-        if (!$boardroom) {
-            // throw
-        }
-
         $em = $this->getDoctrine()->getManager();
-        $appointments = $em->getRepository("AppBundle:Appointment")->findBy(['boardroom' => $boardroom]);
+
+        $boardroomId = $paramFetcher->get('boardroom');
+
+        if ($boardroomId) {
+            $boardroom = $em->getRepository("AppBundle:Boardroom")->find($boardroomId);
+            $appointments = $em->getRepository("AppBundle:Appointment")->findBy(['boardroom' => $boardroom]);
+        } else {
+            $appointments = $em->getRepository("AppBundle:Appointment")->findAll();
+        }
 
         $view = $this->view(['appointments' => $appointments], Response::HTTP_OK);
 
